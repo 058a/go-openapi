d38ac306382ce8b61751259c8dd71a8f60ad8f69
058a <058.auriga@gmail.com>
2024-01-24 16:43:42 +0900

    Add comments generated by codeium

--- internal/hello/usecase.go
+++ internal/hello/usecase.go
@@ -7,6 +7,9 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// GetHello is a function that returns a JSON response with a message "Hello, World!".
+//
+// It takes in a parameter of type echo.Context and returns an error.
 func GetHello(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, &hello_api.Hello{
 		Message: "Hello, World!",
